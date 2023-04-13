80d2fbb72595fce4904f3e62f60faff8c1bc92cd
Eunseo0 <130049914+Eunseo0@users.noreply.github.com>
2023-04-13 19:16:27 +0900

    Update ponketmon.py

--- week2_230413/ponketmon.py
+++ week2_230413/ponketmon.py
@@ -7,7 +7,7 @@ def solution(nums):
       # create new key if num doesn't exist in mon
       if num not in mon:
           mon[num] = 1
-      # if num exists in mon, value+=1
+      # if num exists in mon, value+1
       else:
           mon[num] += 1
   # The length of mon is the maximum number of different types of ponketmon.
